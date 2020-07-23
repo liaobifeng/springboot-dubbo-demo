aed4ab9261333f0df306a35eecd09d96d28e5dcb
van <gu.xu@tuya.com>
2020-07-23 10:08:03 +0800

    设置了zk，现在开了zk能够正常启动

--- provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
+++ provider/src/main/java/com/imooc/springboot/dubbo/demo/provider/DemoServiceImpl.java
@@ -6,6 +6,7 @@ import com.imooc.springboot.dubbo.demo.DemoService;
 @Service
 public class DemoServiceImpl implements DemoService {
 
+    @Override
     public String sayHello(String name) {
         return "Hello, " + name + " (from Spring Boot)";
     }
